9a081c74d977ba7f054abd2c02e71d5313bce928
Jeremy Hart <jeremynhart@gmail.com>
2022-03-20 14:16:49 -0500

    Router Config

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const url = 'mongodb://localhost/UserDBex'
-
 const app = express()
 
 
 
 mongoose.connect(url, {useNewUrlParser:true})
 const con = mongoose.connection
+
 con.on('open', function(){
     console.log('DB connected...')
+})
+
+const dataRouter = require('./routes/api')
+app.use('/api', dataRouter)
+
+app.listen(9000, () => {
+    console.log('Server is listening on PORT:9000')
 })
