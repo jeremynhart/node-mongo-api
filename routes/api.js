1fb82876fb9de0c0f8f3d6c083f991bfae3469d3
Jeremy Hart <jeremynhart@gmail.com>
2022-03-21 13:29:46 -0500

    Final Working Version

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Record = require('../models/schema')
+const Record = require('../dBSchema/schema')
 
 
 // Asynchronous GET Request to fetch ALL RECORDS in the DB
